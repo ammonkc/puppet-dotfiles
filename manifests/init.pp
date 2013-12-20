62daa993bfb4f1aa57a29ea7b0fe0a89011eb399
Ammon Casey <ammon@caseyohana.com>
2013-12-19 14:04:22 -1000

    install dependency packages and change shell to zsh

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,18 @@
 ##############################################################################
 
 class dotfiles {
+    pkg::install { [ 'git', 'tree', 'vim-enhanced', 'zsh', 'bc' ]:
+        stage => pre
+    }
+    user { "vagrant":
+      ensure => present,
+      shell  => '/bin/zsh',
+    }
     file { 'bootstrap':
         ensure  => file,
         path    => '/tmp/bootstrap.sh',
         mode    => 0544,
-        source  => 'puppet:///modules/dotfiles/bootstrap.sh'
+        source  => 'puppet:///modules/dotfiles/bootstrap.sh',
     }
     exec {  "dotfiles bootstrap.sh":
       path      => '/bin:/usr/bin:/sbin:/usr/sbin',
